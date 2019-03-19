3baf7c89e4b23a47d9507d18729753ef71a98d1f
Sébastien Job <sebastien.job@gmail.com>
2019-03-19 11:32:18 +0100

    Fix minor formulae stylistic issues

--- Formula/docker-tools.rb
+++ Formula/docker-tools.rb
@@ -1,9 +1,9 @@
 class DockerTools < Formula
-  desc "Docker-related tools."
+  desc "Docker-related tools"
   homepage "https://github.com/splo/docker-tools"
-  head "https://github.com/splo/docker-tools.git"
   url "https://github.com/splo/docker-tools/archive/v1.0.0.tar.gz"
   sha256 "0957dfbb25da6aa7cca286b05aa1242313372ee57017956038370f8edc4717a5"
+  head "https://github.com/splo/docker-tools.git"
 
   def install
     bin.install "bin/dc-restart"
--- Formula/macos-tools.rb
+++ Formula/macos-tools.rb
@@ -1,11 +1,15 @@
 class MacosTools < Formula
-  desc "Simple command-line utilities only usable on macOS."
+  desc "Simple command-line utilities only usable on macOS"
   homepage "https://github.com/splo/macos-tools"
-  head "https://github.com/splo/macos-tools.git"
   url "https://github.com/splo/macos-tools/archive/v1.0.0.tar.gz"
   sha256 "3cc60f7ddf5b8eec83842c93d8c72f8e657a18a423bcdd2dbf6142c1dabb7590"
+  head "https://github.com/splo/macos-tools.git"
 
   def install
+    if (/darwin/ =~ RUBY_PLATFORM).nil?
+      raise "Expected a macOS OS"
+    end
+
     bin.install "bin/bluetooth-ctl"
     bin.install "bin/delete-dsstores"
     bin.install "bin/flush-cache"
