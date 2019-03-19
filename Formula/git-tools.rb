5da00cb92b6c57961b04d14ab598ce61a8b68ebb
Sébastien Job <sebastien.job@gmail.com>
2019-03-19 11:32:18 +0100

    Fix minor formulae stylistic issues

--- Formula/git-tools.rb
+++ Formula/git-tools.rb
@@ -1,9 +1,9 @@
 class GitTools < Formula
-  desc "Helpful Git tools."
+  desc "Helpful Git tools"
   homepage "https://github.com/splo/git-tools"
-  head "https://github.com/splo/git-tools.git"
   url "https://github.com/splo/git-tools/archive/v1.0.0.tar.gz"
   sha256 "7ae76630bb6845a18528c6a09b15b16bcade1da4a265399b26b8cd7814e7bf3d"
+  head "https://github.com/splo/git-tools.git"
 
   def install
     bin.install "bin/git-delete-merged-branches"
