db156dce8ffa38fdd6d061eb30c5bb9d10e8e42f
Sébastien Job <sebastien.job@gmail.com>
2020-06-15 23:48:10 +0200

    feat: Update git-tools version to 2.0.0

--- Formula/git-tools.rb
+++ Formula/git-tools.rb
@@ -1,17 +1,15 @@
 class GitTools < Formula
   desc "Helpful Git tools"
   homepage "https://github.com/splo/git-tools"
-  url "https://github.com/splo/git-tools/archive/v1.1.0.tar.gz"
-  sha256 "c0402ba159beecd149529a254adb99c0f7f8825feca0eb507f8f42476b588134"
+  url "https://github.com/splo/git-tools/archive/v2.0.0.tar.gz"
+  sha256 "fa5133bc16b52fb080d6c1249678dee7d745019911879cfb1bb16fa161b27c9d"
   head "https://github.com/splo/git-tools.git"
 
   def install
-    bin.install "bin/git-delete-merged-branches"
     bin.install "bin/git-foreach"
   end
 
   test do
-    assert_match "/usr/local/bin/git-delete-merged-branches", shell_output("which git-delete-merged-branches")
     assert_match "/usr/local/bin/git-foreach", shell_output("which git-foreach")
   end
 end
